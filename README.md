1f9b2da41bd40b0d8fa3ad9ddeb09d9d2fa5b5b8
Ryan Bigg <radarlistener@gmail.com>
2009-03-27 13:20:13 +1000

    Version 1 of Numero!

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-Numero
+Numero!
 ======
 
 Write more, do more.
+
+
+Usage
+-----
+
+`numero input.rb` will convert your file to Numero! and run the code. This will also make a file called input.numero.
+
+`numero input.numero` will run your Numero! code.
--- lib/numero.rb
+++ lib/numero.rb
@@ -1,24 +1,30 @@
 require 'optparse'
-OptionParser.new do |opts|
-  # Converts to numero
-  opts.on("-c", "--compile", "Compile numero code") do
-    print "Compiling..."
-    @parsed = true
-    filename = ARGV[0].split(".")[0]
-    File.open(filename + ".numero", "w+") do |file|
-      file.write File.readlines(ARGV[0]).map { |l| l.split("").map { |c| c.unpack("C").first}.join(".")}.join(".")
-    end
-    puts " Done!"
-  end.parse!
+
+def filename(file)
+  file.split(".")[0] + ".numero"
 end
 
+def compile(file)
+  @parsed = true
+  File.open(filename(file), "w+") do |file|
+    file.write File.readlines(ARGV[0]).map { |l| l.split("").map { |c| c.unpack("C").first}.join(".")}.join(".")
+  end
+end
 
 def parse(code)
   eval(code.join("").split(".").map { |c| c.to_i.chr }.join(""))
 end
 
+def to_be_compiled?(file)
+  (File.readlines(file).first.split("").first =~ /\d/).nil?
+end
 
 # For converting the file back from Numero
 if @parsed.nil?
-  parse(File.readlines(ARGV[0]))
+  if to_be_compiled?(ARGV[0])
+    compile(ARGV[0])
+    parse(File.readlines(filename(ARGV[0])))
+  else
+    parse(File.readlines(ARGV[0]))
+  end
 end
